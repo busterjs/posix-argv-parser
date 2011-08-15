5b6386c997430caeec10e97325789bdd6b57f837
August Lilleaas <august@augustl.com>
2011-08-15 20:42:25 +0200

    Adding operations to options.

--- test/buster-args-test.js
+++ test/buster-args-test.js
@@ -1177,3 +1177,59 @@ buster.testCase("buster-args shorthands", {
         }
     }
 });
+
+buster.testCase("Operations", {
+    setUp: function () {
+        this.a = Object.create(busterArgs);
+    },
+
+    "on options": {
+        setUp: function () {
+            this.opt = this.a.createOption("-p");
+        },
+
+        "should set value when successful": function (done) {
+            var self = this;
+            this.opt.operation = function (promise) {
+                promise.resolve("The value");
+            };
+
+            this.a.handle(["-p"], function (errors) {
+                buster.assert.isUndefined(errors);
+                buster.assert.equals(self.opt.value(), "The value");
+                done();
+            });
+        },
+
+        "should set error when failing": function (done) {
+            var self = this;
+            this.opt.operation = function (promise) {
+                promise.reject("The error");
+            };
+
+            this.a.handle(["-p"], function (errors) {
+                buster.refute.isUndefined(errors);
+                buster.assert.match(errors, "The error");
+                buster.assert.isUndefined(self.opt.value());
+                done();
+            });
+        },
+
+        "should leave value intact when resolving with no argument": function (done) {
+            var self = this;
+            var spy = this.spy();
+            this.opt.operation = function (promise) {
+                spy();
+                promise.resolve();
+            };
+            this.opt.hasValue = true;
+
+            this.a.handle(["-p", "1234"], function (errors) {
+                buster.assert.isUndefined(errors);
+                buster.assert.equals(self.opt.value(), "1234");
+                buster.assert(spy.calledOnce);
+                done();
+            });
+        }
+    }
+});
