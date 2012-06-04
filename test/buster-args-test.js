635b891479a8c709c8c85459d0fb77cce83f2593
Christian Johansen <christian@cjohansen.no>
2012-06-04 14:05:16 +0200

    Add expandShorthands method
      - Expands all shorthands across all arguments

--- test/buster-args-test.js
+++ test/buster-args-test.js
@@ -103,5 +103,36 @@ buster.testCase("buster-args", {
         this.a.parse(["--", "--port"], done(function (errors) {
             assert.defined(errors);
         }));
+    },
+
+    "expandShorthands": {
+        setUp: function () {
+            var port = this.a.createOption("-p", "--port");
+            port.hasValue = true;
+            this.a.createOption("-h", "--help");
+            var logLevel = this.a.createOption("-l", "--log-level");
+            logLevel.hasValue = true;
+        },
+
+        "returns arguments untouched when no shorthands": function () {
+            var args = this.a.expandShorthands(["-h", "-p", "1337"]);
+
+            assert.equals(args, ["-h", "-p", "1337"]);
+        },
+
+        "expands shorthand": function () {
+            this.a.addShorthand("-P", ["-p", "80"]);
+            var args = this.a.expandShorthands(["-h", "-P"]);
+
+            assert.equals(args, ["-h", "-p", "80"]);
+        },
+
+        "expands all shorthands": function () {
+            this.a.addShorthand("-P", ["-p", "80"]);
+            this.a.addShorthand("-H", ["--help"]);
+            var args = this.a.expandShorthands(["-H", "-P"]);
+
+            assert.equals(args, ["--help", "-p", "80"]);
+        }
     }
 });
