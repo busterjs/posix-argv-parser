bf1c61af9a287e2d59d92ecbc3fb903d2224fca0
Christian Johansen <christian@cjohansen.no>
2013-04-14 02:51:43 +0200

    Proper test setup

--- run-tests.js
+++ run-tests.js
@@ -1,17 +1,4 @@
 var buster = require("buster-node");
-
-buster.testRunner.onCreate(function (runner) {
-    buster.referee.on("pass", runner.assertionPass.bind(runner));
-
-    runner.on("suite:end", function (results) {
-        setTimeout(function () {
-            process.exit(results.ok ? 0 : 1);
-        }, 50);
-    });
-});
-
-buster.testContext.on("create", buster.autoRun());
-
 require("./test/operand-test");
 require("./test/option-test");
 require("./test/parser-test");
